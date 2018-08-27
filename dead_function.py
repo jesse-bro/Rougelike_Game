7c01bbd23bc814190a0a2b19198c4d8f567276fb
Jesse <jesus.villarreal02@utrgv.edu>
2018-08-27 08:45:14 -0500

    Part 7: Added messages and mesages log. Added HP bar at bottom left.

--- dead_function.py
+++ dead_function.py
@@ -1,14 +1,15 @@
 from game_states import GameStates
+from game_messages import Message
 from render_functions import RenderOrder
 
 def kill_player(player, colors):
     player.char = '%'
     player.color = colors.get('dark_red')
 
-    return 'You died!', GameStates.PLAYER_DEAD
+    return Message('You Died!', colors.get('red')), GameStates.PLAYER_DEAD
 
 def kill_monster(monster, colors):
-    death_message = '{0} is dead!'.format(monster.name.capitalize())
+    death_message = Message('{0} is dead!'.format(monster.name.capitalize()), colors.get('orange'))
 
     monster.char = '%'
     monster.color = colors.get('dark_red')
