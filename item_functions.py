cdca6ffff3456e1a2623490d8a8c45335b47ab76
Jesse <jesus.villarreal02@utrgv.edu>
2018-08-29 11:43:41 -0500

    Part 9: Added Magic Scrolls: Fireball and Lightning. Added Targeting system for Fireball cast.

--- item_functions.py
+++ item_functions.py
@@ -14,3 +14,58 @@ def heal(*args, **kwargs):
         results.append({'consumed': True, 'message': Message('Your wounds start to feel better', colors.get('green'))})
 
     return results
+
+def cast_lightning(*args, **kwargs):
+    caster = args[0]
+    colors = args[1]
+    entities = kwargs.get('entities')
+    game_map = kwargs.get('game_map')
+    damage = kwargs.get('damage')
+    maximum_range = kwargs.get('maximum_range')
+
+    results = []
+
+    target = None
+    closest_distance = maximum_range + 1
+
+    for entity in entities:
+        if entity.fighter and entity != caster and game_map.fov[entity.x,entity.y]:
+            distance = caster.distance_to(entity)
+
+            if distance < closest_distance:
+                target = entity
+                closest_distance = distance
+
+    if target:
+        results.append({'consumed': True, 'target': target, 'message': Message(
+            'A lighting bolt strikes the {0} with a loud thunder! The damage is {1}'.format(target.name, damage))})
+        results.extend(target.fighter.take_damage(damage))
+    else:
+        results.append({'consumed': False, 'target': None, 'message': Message('No enemy is close enough to strike.', colors.get('red'))})
+
+    return results
+
+def cast_fireball(*args, **kwargs):
+    colors = args[1]
+    entities = kwargs.get('entities')
+    game_map = kwargs.get('game_map')
+    damage = kwargs.get('damage')
+    radius = kwargs.get('radius')
+    target_x = kwargs.get('target_x')
+    target_y = kwargs.get('target_y')
+
+    results = []
+
+    if not game_map.fov[target_x,target_y]:
+        results.append({'consumed': False, 'message': Message('You cannot target a tile outside your field of view.', colors.get('yellow'))})
+
+        return results
+
+    results.append({'consumed': True, 'message': Message('The fireball explodes, burning everything withing {0} tiles!'.format(radius), colors.get('orange'))})
+
+    for entity in entities:
+        if entity.distance(target_x,target_y) <= radius and entity.fighter:
+            results.append({'message': Message('The {0} gets burned for {1} hit points.'.format(entity.name,damage), colors.get('orange'))})
+            results.extend(entity.fighter.take_damage(damage))
+
+    return results
--- map_utils.py
+++ map_utils.py
@@ -5,7 +5,8 @@ from components.fighter import Fighter
 from components.ai import BasicMonster
 from components.item import Item
 from render_functions import RenderOrder
-from item_functions import heal
+from item_functions import heal, cast_lightning, cast_fireball
+from game_messages import Message
 
 class GameMap(Map):
     def __init__(self,width,height):
@@ -76,8 +77,19 @@ def place_entities(room, entities, max_monsters_per_room, max_items_per_room, co
         y = randint(room.y1 + 1, room.y2 -1)
 
         if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-            item_component = Item(use_function=heal, amount=4)
-            item = Entity(x,y, '!', colors.get('violet'), 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+            item_chance = randint(0,100)
+
+            if item_chance < 70:
+                item_component = Item(use_function=heal, amount=4)
+                item = Entity(x,y, '!', colors.get('violet'), 'Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+
+            elif item_chance < 85:
+                item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message('Left-click a target tile for the fireball, or right-click to cancel.',
+                                        colors.get('light_cyan')), damage=12, radius=3)
+                item = Entity(x,y,'#', colors.get('red'), 'Fireball Scroll', render_order=RenderOrder.ITEM, item=item_component)
+            else:
+                item_component = Item(use_function=cast_lightning, damage=20, maximum_range=5)
+                item = Entity(x,y,'#', colors.get('yellow'), 'Lightning Scroll', render_order=RenderOrder.ITEM, item=item_component)
 
             entities.append(item)
 
